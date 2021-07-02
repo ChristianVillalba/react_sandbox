736c3286805a4ec3bbda80780a5dd7a6cdba3c4e
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-07-02 13:13:34 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>This is React!</h1>
+    <p>Everything inside this div counts as a single HTML Element.</p>
+    <p>Testing Sandbox on GitHub.</p>
+  </div>,
+  document.getElementById("root")
+);
